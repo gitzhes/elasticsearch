36acdab5d62e3effc694b3ce62da27fc6b6596c6
root <xianzhihai@gmail.com>
2019-03-12 15:15:03 +0800

    k8s version

---
+++ es-persistence/createPvs.sh
@@ -0,0 +1,5 @@
+kubectl create -f pv-esmaster01.yaml
+kubectl create -f pv-esmaster02.yaml
+kubectl create -f pv-esmaster03.yaml
+kubectl create -f pv-esdata01.yaml
+kubectl create -f pv-esdata02.yaml
---
+++ es-persistence/deletePVs.sh
@@ -0,0 +1,5 @@
+kubectl delete pv esdata01-volume
+kubectl delete pv esdata02-volume
+kubectl delete pv esmaster01-volume
+kubectl delete pv esmaster02-volume
+kubectl delete pv esmaster03-volume
